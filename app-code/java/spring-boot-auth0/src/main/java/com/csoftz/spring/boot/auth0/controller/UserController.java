0e6012aa8f4649682f46e4387c98ec57183a1a9a
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2017-05-19 06:34:51 -0500

    Adds configuration and many more changes

---
+++ app-code/java/spring-boot-auth0/src/main/java/com/csoftz/spring/boot/auth0/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.csoftz.spring.boot.auth0.controller;
+
+/**
+ * Created by csoftzusr on 3/20/17.
+ */
+public class UserController {
+}
---
+++ app-code/java/spring-boot-auth0/src/main/java/com/csoftz/spring/boot/auth0/security/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.csoftz.spring.boot.auth0.security;
+
+/**
+ * Created by csoftzusr on 3/20/17.
+ */
+public class WebSecurityConfig {
+}
